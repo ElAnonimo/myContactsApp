71d70e74e8a96bf7d77869cdfa59157d3ea9dec9
Valentyn Kolesnikov <javadev75@gmail.com>
2016-04-18 19:36:19 +0300

    Improve integration test for contacs page.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,36 +7,36 @@ describe('my app', function() {
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/contacts");
   });
 
 
-  describe('view1', function() {
+  describe('contacts', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/contacts');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render view1 when user navigates to /contacts', function() {
+      expect(element.all(by.css('body > div > div.row > div > h1')).first().getText()).
+        toMatch(/myContacts/);
     });
 
   });
+
+
+  // describe('view2', function() {
+
+  //   beforeEach(function() {
+  //     browser.get('index.html#/view2');
+  //   });
+
+
+  //   it('should render view2 when user navigates to /view2', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 2/);
+  //   });
+
+  // });
 });
